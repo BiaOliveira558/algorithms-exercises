c51c97aa7f5bc1372cb70f17b3cdefaa3cf7572f
BiaOliveira558 <beatrizoliveirabarros255@gmail.com>
2025-09-15 22:28:21 -0300

    print test cases

---
+++ vjudge/competitiva/print_test_cases.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int i = 1;
+
+    while (true) {
+    int input;
+    cin >> input;
+
+    if (input == 0) break; 
+
+    cout << "Case "<< i <<": "<< input << "\n";
+    i++;
+    }
+
+
+    return 0;
+}
